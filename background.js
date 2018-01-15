423506d3dd0ce5a7744b1140f4469c4287d55189
ruan.sheng <ruan.sheng@immomo.com>
2018-01-15 19:37:25 +0800

    init

---
+++ background.js
@@ -0,0 +1,37 @@
+﻿chrome.browserAction.onClicked.addListener(function(activeTab)
+{
+    chrome.tabs.create({ url: 'index.html'});
+});
+chrome.runtime.onInstalled.addListener(function (object) 
+{
+    chrome.tabs.create({ url: 'index.html' });
+});
+
+chrome.extension.onMessage.addListener(
+	function (request, sender, sendResponse) {
+		
+		curTab = sender.tab;
+		chrome.windows.get(sender.tab.windowId, {populate: true}, function (window) {
+			curWindow = window;
+			if(request.cmd=="move"){
+				activeMove();
+			}
+		});		
+});
+
+function activeMove(){
+	//最后一个了
+	if(curTab.index == curWindow.tabs.length-1){
+		for (var i = 0 in curWindow.tabs) {
+			if(curWindow.tabs[i].index == curTab.index-4){
+				chrome.tabs.update(curWindow.tabs[i].id, {active: true});
+			}
+		}
+	} else{
+		for (var i = 0 in curWindow.tabs) {
+			if(curWindow.tabs[i].index == curTab.index+1){
+				chrome.tabs.update(curWindow.tabs[i].id, {active: true});
+			}
+		}
+	}
+}
---
+++ content.js
@@ -0,0 +1,27 @@
+﻿
+function gototab(where){
+	chrome.extension.sendMessage({cmd: where}, function (response) {
+		;
+	});
+}
+
+function domap(keyCode){
+	setTimeout(function(){
+		var keycodes = new Array(48,49,50,51,52,53,54,55,56, 97,98,99,100,101,102,103,104,105);
+		for(var i = 0; i < keycodes.length; i++) {
+			if(keyCode == keycodes[i]) {
+				gototab("move");				
+			}
+		}
+	},50);
+}
+
+function main(){
+	document.onkeyup = function (event) {
+			var e = event || window.event;
+			var keyCode = e.keyCode || e.which;
+			domap(keyCode);
+     }
+}
+
+main();
